d26bdf2e01b28af3c0bffcc8dd23c2a1dc2eca00
cberdejo <cberdejo@uma.es>
2025-07-03 17:49:06 +0200

    chore: __init__.py created

---
+++ utils-project/src/db_models/__init__.py
@@ -0,0 +1,11 @@
+from .town import Town
+from .intangible import Intangible
+from .real_estate import RealEstate
+from .image_town import ImageTown
+
+__all__ = [
+    "Town",
+    "IntangibleAsset",
+    "RealEstateAsset",
+    "ImageTown"
+]
