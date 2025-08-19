d15e6509220a859b4a09d54f515b37ec52cdb004
Christian Berdejo <cberdejo2205@gmail.com>
2025-08-19 14:31:38 +0200

    fix(scraper): correct task import paths to pipeline.tasks.* to match package layout

--- scraper/src/pipeline/main.py
+++ scraper/src/pipeline/main.py
@@ -5,16 +5,18 @@ from prefect.task_runners import ConcurrentTaskRunner
 from models.municipality import MunicipalityInfo
 
 
-from tasks.scrape_from_turismo_andalucia import get_towns_info_from_turismo_andalucia
-from tasks.get_andalusia_towns_ubi_and_name import get_municipality_name_and_ubi
-from tasks.get_info_from_iaph import get_info_from_iaph
-from scraper.src.pipeline.tasks.merge_municipality_info import (
-    build_municipality_info_list,
+from pipeline.tasks.scrape_from_turismo_andalucia import (
+    get_towns_info_from_turismo_andalucia,
 )
-from tasks.wikipedia_beach_check import get_towns_with_beaches_from_wikipedia
-from tasks.generate_embeddings import generate_embeddings
-from tasks.save_data import load_info_to_postgres
-from tasks.upload_report import save_task_metadata_to_minio
+from pipeline.tasks.get_andalusia_towns_ubi_and_name import (
+    get_municipality_name_and_ubi,
+)
+from pipeline.tasks.get_info_from_iaph import get_info_from_iaph
+from pipeline.tasks.merge_municipality_info import build_municipality_info_list
+from pipeline.tasks.wikipedia_beach_check import get_towns_with_beaches_from_wikipedia
+from pipeline.tasks.generate_embeddings import generate_embeddings
+from pipeline.tasks.save_data import load_info_to_postgres
+from pipeline.tasks.upload_report import save_task_metadata_to_minio
 
 
 @flow(
